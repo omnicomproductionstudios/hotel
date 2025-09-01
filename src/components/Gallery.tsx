98392cc97d29633dd6935cce331737cd5f3e60cd
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-09-01 22:26:11 +0530

    new change

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Link from "next/link"
+
 
 export default function Gallery(){
     return(
@@ -14,34 +16,52 @@ export default function Gallery(){
                     <div className="row g-4">
                         <div className="col-lg-8 col-md-6">
                             <div className="image">
-                                <img src="/images/room.jpg" alt="image" />
-                                <div className="copy">
-                                    <span>Premium Cottage</span>
-                                </div>
+                                <Link href="/gallery">
+                                    <img src="/images/room.jpg" alt="image" />
+                                    <div className="copy">
+                                        <span>Premium Cottage</span>
+                                    </div>
+                                </Link>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6">
                             <div className="image">
-                                <img src="/images/room1.jpg" alt="image" />
-                                <div className="copy">
-                                    <span>Reception</span>
-                                </div>
+                                <Link href="/gallery">
+                                    <img src="/images/room1.jpg" alt="image" />
+                                    <div className="copy">
+                                        <span>Reception</span>
+                                    </div>
+                                </Link>
                             </div>
                         </div>
                         <div className="col-lg-4 col-md-6">
                             <div className="image">
-                                <img src="/images/room4.jpg" alt="image" />
-                                <div className="copy">
-                                    <span>Restaurant</span>
-                                </div>
+                                <Link href="/gallery">
+                                    <img src="/images/room4.jpg" alt="image" />
+                                    <div className="copy">
+                                        <span>Restaurant</span>
+                                    </div>
+                                </Link>
                             </div>
                         </div>
-                        <div className="col-lg-8 col-md-6">
+                        <div className="col-lg-4 col-md-6">
                             <div className="image">
-                                <img src="/images/room3.jpg" alt="image" />
-                                <div className="copy">
-                                    <span>Swimming Pool</span>
-                                </div>
+                                <Link href="/gallery">
+                                    <img src="/images/room3.jpg" alt="image" />
+                                    <div className="copy">
+                                        <span>Swimming Pool</span>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div className="image">
+                                <Link href="/gallery">
+                                    <img src="/images/acti2.jpg" alt="image" />
+                                    <div className="copy">
+                                        <span>Play Zone</span>
+                                    </div>
+                                </Link>
                             </div>
                         </div>
                     </div>
